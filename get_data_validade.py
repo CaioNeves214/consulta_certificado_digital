86060ef60e29cb0f64fcd402d08a9d2cd3a4b221
CaioNeves214 <workspace2144@gmail.com>
2025-07-16 12:42:40 -0300

    Tratamento de erros e Inserção de dados no banco

--- get_data_validade.py
+++ get_data_validade.py
@@ -2,15 +2,23 @@ from cryptography.hazmat.primitives.serialization import pkcs12
 from con_database import con_banco_local, con_banco_remoto
 from pathlib import Path
 
-def get_empresas(cursor):
+def fechar_conexoes(local_cursor, local_connection, remote_cursor, remote_connection):
+    """Fecha as conexoes com os bancos de dados"""
+    try:
+        local_cursor.close()
+        local_connection.close()
+        remote_cursor.close()
+        remote_connection.close()
+    except Exception as e:
+        print(f"Erro ao fechar conexões: {e}")
+
+def get_empresas_local(cursor):
     """Consulta para obter os dados das empresas"""
     try: 
-        cursor.execute("SELECT CGC, SENHACERTIFICADO, CAMINHOCERTIFICADO FROM empresas;")
-    except Exception as e:
-        print(f"Erro ao executar scriptSQL: {e}")
-        return []
-    finally:
+        cursor.execute("SELECT CGC, SENHACERTIFICADO, CAMINHOCERTIFICADO, CONCAT(CODIGO_N, ' - ', RAZAO), NOME FROM empresas;")
         return cursor.fetchall()
+    except Exception as e:
+        print(f"Erro de Script SQL: {e}")
 
 def get_validade_certificado(cert_path, password):
     """Obtem a validade do certificado"""
@@ -22,7 +30,15 @@ def get_validade_certificado(cert_path, password):
         return cert[1].not_valid_after_utc.date()
     except Exception as e:
         print(f"Erro ao ler o certificado: {e}")
-        return None
+
+def update_validade_certificado(remote_cursor, remote_connection, cnpj, validade_cert, razao, nome):
+    """Atualiza ou insere empresa com validade do certificado"""
+    remote_cursor.execute("SELECT CGC FROM rotina_cliente WHERE CGC = %s", (cnpj,))
+    if remote_cursor.fetchone() == None:
+        remote_cursor.execute("INSERT INTO rotina_cliente (CGC, validade_certificado, RAZAO, NOME) VALUES (%s, %s, %s, %s)", (cnpj, validade_cert, razao, nome))
+    else:
+        remote_cursor.execute("UPDATE rotina_cliente SET validade_certificado = %s WHERE CGC = %s", (validade_cert, cnpj))
+    remote_connection.commit()
 
 def main():
     """Funcao principal para obter validade do certificado da empresa"""
@@ -31,18 +47,25 @@ def main():
     local_connection, local_cursor = con_banco_local()
     remote_connection, remote_cursor = con_banco_remoto()
 
-    empresas = get_empresas(local_cursor)
-    for cnpj, password, cert_path in empresas:
+    if get_empresas_local(local_cursor) == None:
+        print("Nenhuma empresa encontrada")
+        fechar_conexoes(local_cursor, local_connection, remote_cursor, remote_connection)
+        return
+    else:
+        empresas = get_empresas_local(local_cursor)
+    
+    for cnpj, password, cert_path, razao, nome in empresas:
         if not cert_path:
             continue
         cert_path = Path(cert_path)
         validade_cert = get_validade_certificado(cert_path, password)
-        print(f"CNPJ: {cnpj}, Validade do Certificado: {validade_cert}")
+        if not validade_cert:
+            print(f"Certificado inválido ou não encontrado para CNPJ: {cnpj}")
+            return
+        
+        update_validade_certificado(remote_cursor, remote_connection, cnpj, validade_cert, razao, nome)
     
-    local_cursor.close()
-    local_connection.close()
-    remote_cursor.close()
-    remote_connection.close()
+    fechar_conexoes(local_cursor, local_connection, remote_cursor, remote_connection)
 
 if __name__ == "__main__":
     main()
